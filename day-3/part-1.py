ca33692c3e1dc3aa8a9f3b9630d5458dbf06422d
M. Hofman <m.hofman@chronos-technology.nl>
2017-12-04 21:22:24 +0100

    Finished day 3

---
+++ day-3/part-1.py
@@ -0,0 +1,8 @@
+inputString = 265149
+
+# Bottom right is of the spiral is square root of the the odd integer sequence
+# 515 * 515 = 265225
+# 515 is the 257 number in the sequence
+# Need to count back 265225 - 265149 = 106 places
+# Coordinates are (181, -257)
+# 181 + 257 = 438
---
+++ day-3/part-2.py
@@ -0,0 +1,4 @@
+inputString = 265149
+
+# See OEIS: https://oeis.org/A141481
+# 266330
